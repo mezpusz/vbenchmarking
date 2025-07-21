564dabd62b40dfbf1f1cec5f141f6baa88a86c7b
Marton Hajdu <mezpusz@hotmail.com>
2025-07-21 09:43:45 +0200

    Put induction regression test into one suite

--- compare_branches.py
+++ compare_branches.py
@@ -7,6 +7,7 @@ BUILDDIR = os.path.join(VAMPIREDIR, 'cmake-build')
 BENCHMARKINGDIR = '/home/mhajdu/vbenchmarking'
 
 def run_cmd(cmd, cwd=None):
+  print(f'running {cmd}')
   try:
     subprocess.check_call(cmd, shell=True, cwd=cwd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
   except subprocess.CalledProcessError as e:
@@ -27,7 +28,10 @@ class Runner:
     return len(subprocess.check_output(f'git ls-remote --heads origin "refs/heads/{args.branch1}"', shell=True, cwd=VAMPIREDIR)) > 0
 
   def result_file(self):
-    return f'results/{self.benchmark}.{self.branch}.{time.strftime("%Y-%m-%d_%H-%M-%S", self.timestamp)}.results.{self.run}.xml.bz2'
+    runStr = ""
+    if self.run:
+      runStr = f'.{self.run}'
+    return f'results/{self.benchmark}.{self.branch}.{time.strftime("%Y-%m-%d_%H-%M-%S", self.timestamp)}.results{runStr}.xml.bz2'
 
   def build_and_run(self):
     print(f'building {self.branch}...')
@@ -38,12 +42,16 @@ class Runner:
     run_cmd('make -j60', BUILDDIR)
     subprocess.check_output('./vampire --version', shell=True, cwd=BUILDDIR)
 
+    runOption = ""
+    if self.run:
+      runOption = f'-r "{self.run}"'
+
     print(f'running {self.branch}...')
     run_cmd(f'benchexec --no-container \
               --numOfThreads 60 \
               --tool-directory "{BUILDDIR}" \
               --name "{self.branch}" \
-              -r "{self.run}" \
+              {runOption} \
               --startTime "{time.strftime("%Y-%m-%d %H:%M:%S", self.timestamp)}" \
               "{os.path.join(BENCHMARKINGDIR, "benchmarks", self.benchmark)}.xml"')
     print(f'result file is {self.result_file()}')
